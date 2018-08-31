1217a1ef09b69e95c596756399f7eb02da5ec204
dpena <deivi_bass@hotmail.com>
2018-08-31 14:15:11 -0400

    Adding missing doc blocks

--- src/Config/ConfigAwareInterface.php
+++ src/Config/ConfigAwareInterface.php
@@ -6,7 +6,24 @@ interface ConfigAwareInterface
 {
     const ENVIRONMENT = 'local';
 
+    /**
+     * Sets the config enviroment variables.
+     *
+     * @todo Should return void.
+     *
+     * @param array $config A asociative array containing the config variables.
+     *
+     * @return void
+     */
     public function setConfig(array $config);
 
+    /**
+     * Gets a config enviroment variable by it's unique key.
+     *
+     * @param string $key     The key to search for.
+     * @param mixed  $default The defualt value to return if the key is not found.
+     *
+     * @return mixed The config value.
+     */
     public function getConfig(string $key, $default = null);
 }

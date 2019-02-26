7c0a5424ae617bed67cc6c4abf96c6a469e81b1e
Deivi Peña <systemson@gmail.com>
2019-02-26 14:27:09 -0400

    Updates

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -5,15 +5,29 @@ namespace Amber\Config;
 use Amber\Collection\Collection;
 use Amber\Filesystem\Filesystem;
 use Amber\Utils\Implementations\AbstractWrapper;
+use Amber\Utils\Traits\ArgumentAwareTrait;
+use Amber\Utils\Contracts\ArgumentAwareInterface;
 
 /**
  * Config provider class.
  *
  * This class stores and shares configurations throughout the components.
  */
-class Config extends AbstractWrapper
+class Config extends AbstractWrapper implements ArgumentAwareInterface
 {
+    use ArgumentAwareTrait;
+
     protected static $accessor = Collection::class;
+
+    /**
+     * Runs before the class constructor.
+     *
+     * @return void
+     */
+    public static function beforeConstruct(): void
+    {
+        static::setArguments([], true);
+    }
 
     public static function loadFromFile()
     {
